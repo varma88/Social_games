74cab9f92a6f8056da070dd257a39a852f5fb6c3
varma88 <59354639+varma88@users.noreply.github.com>
2022-04-16 02:27:45 +0530

    test-2

---
+++ sentence_reconstruction/__init__.py
@@ -0,0 +1,37 @@
+
+from otree.api import *
+c = cu
+
+doc = ''
+class C(BaseConstants):
+    NAME_IN_URL = 'sentence_reconstruction'
+    PLAYERS_PER_GROUP = 3
+    NUM_ROUNDS = 1
+    SENTENCE = 'I find the words'
+class Subsession(BaseSubsession):
+    pass
+class Group(BaseGroup):
+    reconstructed_sentence = models.StringField()
+def aggregate(group: Group):
+    players = group.get_players()
+    group.reconstructed_sentence = [p.remembered_sentence for p in players]
+    
+    
+class Player(BasePlayer):
+    Name = models.StringField()
+    remembered_sentence = models.StringField(blank=True)
+    final_sentence = models.StringField()
+class Sentence(Page):
+    form_model = 'player'
+    timeout_seconds = 10
+class Individual_memory(Page):
+    form_model = 'player'
+    form_fields = ['Name', 'remembered_sentence']
+class MyWaitPage(WaitPage):
+    after_all_players_arrive = aggregate
+class Group_memory(Page):
+    form_model = 'player'
+    form_fields = ['final_sentence']
+class Results(Page):
+    form_model = 'player'
+page_sequence = [Sentence, Individual_memory, MyWaitPage, Group_memory, Results]
---
+++ wisdom_of_the_crowd/__init__.py
@@ -0,0 +1,36 @@
+
+from otree.api import *
+c = cu
+
+doc = ''
+class C(BaseConstants):
+    NAME_IN_URL = 'wisdom_of_the_crowd'
+    PLAYERS_PER_GROUP = 3
+    NUM_ROUNDS = 1
+    ACTUAL_NUMBER = 200
+class Subsession(BaseSubsession):
+    pass
+class Group(BaseGroup):
+    av_guess = models.FloatField()
+def average_guess(group: Group):
+    players = group.get_players()
+    guesses = [p.guess for p in players]
+    group.av_guess = sum(guesses) / C.PLAYERS_PER_GROUP
+    
+class Player(BasePlayer):
+    guess = models.FloatField(label='How many balls do you think there are in the box?')
+    devn = models.FloatField()
+def deviation(player: Player):
+    player.devn = player.guess - C.ACTUAL_NUMBER
+class Guess(Page):
+    form_model = 'player'
+    form_fields = ['guess']
+    timeout_seconds = 10
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        deviation(player)
+class MyWaitPage(WaitPage):
+    after_all_players_arrive = average_guess
+class Results(Page):
+    form_model = 'player'
+page_sequence = [Guess, MyWaitPage, Results]
