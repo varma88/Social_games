5a4f8fe428a813e30183021ef499775779e0f4c1
nigradwohl <nico.gradwohl@uni.kn>
2022-05-12 09:31:47 +0200

    implement dynamic highchart

--- wisdom_of_the_crowd/__init__.py
+++ wisdom_of_the_crowd/__init__.py
@@ -17,8 +17,19 @@ class Group(BaseGroup):
 def average_guess(group: Group):
     players = group.get_players()
     guesses = [p.guess for p in players]
-    group.av_guess = sum(guesses) / C.PLAYERS_PER_GROUP
+    av_guess = sum(guesses) / C.PLAYERS_PER_GROUP
+    group.av_guess = av_guess
     group.av_devn = group.av_guess - C.ACTUAL_NUMBER
+    # Save the info in highchart format for later use.
+    # Get categories for chart (actually currently constant per group):
+    hcats = ["Player" + str(id) for id in range(1, C.PLAYERS_PER_GROUP + 1)] + \
+            ['Average guess', 'Actual number']
+    hdat = guesses + [av_guess] + [C.ACTUAL_NUMBER]  # list of data in appropriate order.
+    # Assign the variable to each participant (player across rounds) for later use:
+    # TODO: Maybe there is an option to access he group object in js_vars (instead of accessing participant vars)-- I did not see any.
+    for p in players:
+        p.participant.high_cats = hcats
+        p.participant.high_data = [{'name': ix, 'data': [dat]} for ix, dat in zip(hcats, hdat)]
     
 class Player(BasePlayer):
     guess = models.FloatField(label='How many balls do you think there are in the box?')
@@ -39,12 +50,19 @@ class Guess(Page):
         
 class MyWaitPage(WaitPage):
     after_all_players_arrive = average_guess
-    @staticmethod
-    def js_vars(player):
-        my_dict = {}
-        my_dict = {player.id_in_group: player.guess}       
-        return my_dict
+
         
 class Results(Page):       
     form_model = 'player'
+
+    @staticmethod
+    def js_vars(player):
+        my_dict = {}  # no need to initialize empty dict.
+        # my_dict = {player.id_in_group: player.guess}
+        # Create dictionary:
+        my_dict = {'highchart_cat': player.participant.high_cats,
+                   'highchart_dat': player.participant.high_data,
+                   }
+        print("@@@@ my_dict", my_dict)  # print statement for debugging.
+        return my_dict
 page_sequence = [Intro, Guess, MyWaitPage, Results]
